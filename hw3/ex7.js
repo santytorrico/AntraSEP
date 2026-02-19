b309fcc457c729c37a10f12fc9f0d1f6bd1d6528
santytorrico <santy.torrico@gmail.com>
2026-02-19 12:52:33 -0400

    feat: implement logic to count vowels in a string

---
+++ hw3/ex7.js
@@ -0,0 +1,14 @@
+// Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string. 
+// Note: As the letter 'y' can be regarded as both a vowel and a consonant, we do not count 'y' as vowel here. 
+// Example string: 'The quick brown fox' 
+// Expected Output: 5
+function numberOfVowels(sent){
+    let arr = sent.toLowerCase().split("");
+    let validVowels = ["a", "e", "i", "o", "u"]
+    const vowels = arr.filter(vowel =>
+        "aeiou".includes(vowel)
+    )
+    return vowels.length;
+}
+sent = "The quick brown fox"
+console.log(numberOfVowels(sent))
