08a8177955ef293251f1506f9e897339269006f0
santytorrico <santy.torrico@gmail.com>
2026-02-19 12:54:09 -0400

    feat: add logic to check the type of an argument

---
+++ hw3/ex9.js
@@ -0,0 +1,6 @@
+// Write a JavaScript function which accepts an argument and returns the type. 
+// Note: There are six possible values that typeof returns: object, boolean, function, number, string, and undefined.
+function typeCheck(arg){
+    return typeof(arg);
+}
+console.log(typeCheck())
