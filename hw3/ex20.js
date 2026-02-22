50eee35f825e92d8777283d0524960d4bcc9c7ec
santytorrico <santy.torrico@gmail.com>
2026-02-22 18:05:23 -0400

    feat: add function to generate a string from a list of random characters

---
+++ hw3/ex20.js
@@ -0,0 +1,11 @@
+// Write a JavaScript function that generates a string id (specified length) of random characters. 
+// Sample character list: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+function createRandomString(length) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let str = "";
+  for (let i = 0; i < length; i++) {
+    str += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return str;
+}
+console.log(createRandomString(8)); // e.g., 'B5cgHjsT'
