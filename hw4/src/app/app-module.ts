dca21a86b67b653152b12dcad1004c42d93bb6ab
santytorrico <santy.torrico@gmail.com>
2026-03-10 14:21:30 -0300

    feat: implement user profile component with input binding and save counter

--- hw4/src/app/app-module.ts
+++ hw4/src/app/app-module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { UserProfile } from './user-profile/user-profile';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [App, UserProfile],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [App, UserProfile,],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
--- hw4/src/app/user-profile/user-profile.ts
+++ hw4/src/app/user-profile/user-profile.ts
@@ -6,4 +6,31 @@ import { Component } from '@angular/core';
   templateUrl: './user-profile.html',
   styleUrl: './user-profile.scss',
 })
-export class UserProfile {}
+export class UserProfile {
+  name: string = '';
+  email: string = '';
+  dateOfBirth: string = '';
+  saveCount: number = 0;
+
+  resetForm() {
+    this.name = '';
+    this.email = '';
+    this.dateOfBirth = '';
+  }
+
+  saveProfile() {
+    const profile = {
+      name: this.name,
+      email: this.email,
+      dateOfBirth: this.dateOfBirth
+    };
+
+    console.log(profile);
+    this.saveCount++;
+  }
+
+  isEmpty(): boolean {
+    return !this.name && !this.email && !this.dateOfBirth;
+  }
+
+}
